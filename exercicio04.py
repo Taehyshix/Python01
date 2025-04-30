936198f517060aaa551fbe3094b5cdb9d6d60248
Jessica Loris <dearjess535@gmail.com>
2025-04-30 11:01:21 -0300

    exericio4

--- exercicio04.py
+++ exercicio04.py
@@ -3,4 +3,5 @@ produto = input("Digite o nome do produto: ")
 valor = float(input("Digite o valor do produto: "))
 quantidade = int(input("Digite a quantidade no estoque: "))
 
-estoque(produto,quantidade,valor)
+print(estoque(produto,quantidade,valor))
+
